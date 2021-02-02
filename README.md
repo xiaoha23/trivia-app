6e9139dcc668b1cc2d300a22f2c46ba6cfdd2b60
Vyom <vyom.reloaded@gmail.com>
2021-02-02 18:47:07 -0500

    Initialize repository

---
+++ README.md
@@ -0,0 +1,15 @@
+### Trivia APP API
+
+** POST /api/v1/trivia/questions **
+
+** GET /api/v1/trivia/questions ** 
+
+** GET /api/v1/trivia/questions?amount=10**
+
+
+### Profiles 
+
+- default profile
+- test profile
+- prod profile
+
---
+++ src/test/java/com/galvanize/triviaapp/TriviaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.triviaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
